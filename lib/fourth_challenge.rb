1ac4a8c957adf16778e295b28fcdd032e6cb503c
Thomas Edwards <thce28@gmail.com>
2019-02-10 14:48:31 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,7 +6,8 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         {name: "Benvolio", age: "17", attitude: "worried"},
-        {name: "Mercutio", age: "18", attitude: "hot_headed"}      ]
+        {name: "Mercutio", age: "18", attitude: "hot_headed"}
+        ]
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
